1929c5e9973dba783f904002c806f4f4e7083756
yancey <VictoryNo1@users.noreply.github.com>
2021-04-02 00:21:11 +0800

    ddd
    Ddd

--- Python/test_python.py
+++ Python/test_python.py
@@ -1,2 +1,2 @@
 line = "public char getColor ( ) throws java.io.IOException { VAR_1 . METHOD_1 ( ( STRING_1 + \"\n\" ) ) ; java.lang.String response = VAR_2 . METHOD_2 ( ) ; java.lang.System.out.println ( response ) ; return response . METHOD_3 ( 0 ) ; }"
-print(len(line.split()))
+print(len(line.split()#45677

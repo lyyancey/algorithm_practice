85dc2acc843caffe189f7e4ef14e22d6c29f60f5
yancey <VictoryNo1@users.noreply.github.com>
2021-04-02 09:59:03 +0800

    更新了每日一题
    更新了每日一题的内容

--- C/面试题17.21.c
+++ C/面试题17.21.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
 int trap(int* height, int heightSize){
+    int i=0;
+    while(height[i]!=0)i++;
+    for(int k = i+1;height[k]>=height[i];k++){
+        
+    }
     
 }
 int main(){

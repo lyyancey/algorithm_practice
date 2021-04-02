7298dbe5f6b9c2ab8c9730a7509be8a1df1cdfb9
Yancey <yancey@qq.com>
2021-04-02 14:11:44 +0800

    完成了每日一题

--- C/面试题17.21.c
+++ C/面试题17.21.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 
 int trap(int* height, int heightSize){
-    int i=0;
-    while(height[i]!=0)i++;
-    for(int k = i+1;height[k]>=height[i];k++){
-        
+    int sum = 0,left = 0, right = heightSize-1;
+    int volumel =0, high = 1;
+    for (int i=0;i<heightSize;i++){
+        sum += height[i];
     }
-    
+    while(left <= right){
+        while(left<=right&&height[left]<high)left++;
+        while (left<=right&&height[right]<high)right--;
+        volumel += right-left+1;
+        high++;
+    }
+    return volumel-sum;
 }
 int main(){
-
+    int a[12] = {0,1,0,2,1,0,1,3,2,1,2,1}, height = 12;
+    int x = trap(a, 12);
+    printf("%d\n",x);
+    return 0;
 }

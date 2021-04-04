56e5b4a761f9bc5c10335e14359e5265cc4ddd00
Yancey <yancey@qq.com>
2021-04-04 16:58:58 +0800

    更新无重复最长字串

--- C/无重复字符的最长子串.c
+++ C/无重复字符的最长子串.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 */
@@ -7,43 +8,18 @@ int length(char *s){
     while(s[i]!='\0')i++;
     return i;
 }
-typedef struct Set{
-    char *set;
-    int len;
-    int contains(char c){
-        int x = ((int)c)%len;
-        if(set[x]!='\0')return 1;
-        return 0;
-    }
-    void remove(char c){
-        int x = ((int)c)%len;
-        set[x] = '\0';
-    }
-    void add(char c){
-        int x = ((int)c)%len;
-        set[x] = c;
-    }
-    void init(int l){
-        len = l;
-        set = (char*)malloc(sizeof(char)*l);
-        for(int i=0;i<l;i++){
-            set[i]='\n'
-        }
-    }
-}Set;
 int lengthOfLongestSubstring(char * s){
     int len = length(s);
-    Set set;
-    set.init(len);
+    char* ss = (char*)malloc(sizeof(char)*len);
+    for(int i=0;i<len;i++)ss[i]='\0';
     int r = 0,rk=-1;
     int rs = 0;
-    for(int i = 0;i<len;i++)set[i]='\0';
     for(int i=0;i<len;i++){
         if(i!=0){
-           set.remove(s[i]);
+           ss[((int)s[i])%len] = '\0';
         }
-        for((rk+1<len)&&(!set.contains(set.contains(rk+1)))){
-            set.add(s[rk+1]);
+        while((rk+1<len)&&(ss[((int)s[rk+1])%len]!='\0')){
+            ss[(int)s[rk+1]%len]=s[rk+1];
             rk++;
         }
         if(rs>(rk-i+1))rs = rk-i+1;

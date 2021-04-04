c2cdc162d6bb535e1645d0ef8873cf3cefbec3b8
Yancey <yancey@qq.com>
2021-04-04 19:46:13 +0800

    完成无重复字符的最长字串

--- Java/3.java
+++ Java/3.java
@@ -1,6 +1,24 @@
-class Solution{
-    public int lengthOfLongestSubstring(String s){
-        int k=0,rk=-1;
-        
+import java.util.*;
+class Problem3 {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<Character>();
+        int n = s.length();
+        int res = 0,rk =0;
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                set.remove(s.charAt(i-1));
+            }
+            while(rk<n&&!set.contains(s.charAt(rk))){
+                set.add(s.charAt(rk));
+                ++rk;
+            }
+            res = res>(rk-i)?res:(rk-1);
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        Problem3 s = new Problem3();
+        int x = s.lengthOfLongestSubstring("pwwkew");
+        System.out.println(x);
     }
 }

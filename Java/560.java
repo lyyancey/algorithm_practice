821816b1395f61052f597fed9a43a6e52df034bd
Yancey <yancey@qq.com>
2021-04-08 14:56:33 +0800

    添加了注释

--- Java/560.java
+++ Java/560.java
@@ -5,11 +5,13 @@ class Problem560 {
         int ans = 0,li = 0;
         map.put(0, 1);
         for(int i = 0;i<nums.length;i++){
-            li += nums[i];
+            li += nums[i];//统计每一个前缀
+            //查看map里面是不是有需要的前缀，如果有，那有几个就说明在当前情况下问题有几个解
             int lj = li-k;
             if(map.containsKey(lj)){
                 ans += map.get(lj);
             }
+            //将当前的前缀放入map
             map.put(li,map.getOrDefault(li, 0)+1);
         }
         return ans;

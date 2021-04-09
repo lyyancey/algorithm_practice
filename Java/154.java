9c2c9e08e15631e72e5876b1ffa7f301fd291741
Yancey <yancey@qq.com>
2021-04-09 16:02:33 +0800

    添加了每日一题的解析

--- Java/154.java
+++ Java/154.java
@@ -1,14 +1,16 @@
 class Problem154{
    public int findMin(int[] nums){
-      int left = 0, right = nums.length()-1;
+      int left = 0, right = nums.length-1;
       while(left<right){
          int mid = left + (right - left)/2;
-         if(nums[mid]>=right){
+         if(nums[mid]<nums[right]){
             right = mid;
-         }else{
-            left = mid;
+         }else if(nums[mid]>nums[right]){
+            left = mid+1;
+         }else if(nums[right]==nums[mid]){
+            right--;
          }
       }
-      return left;
+      return nums[left];
    } 
 }

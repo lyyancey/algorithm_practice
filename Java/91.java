c3451fa4a3cd5b4d9c971682a16c46248ac0b7e1
Yancey <yancey@qq.com>
2021-04-21 17:04:03 +0800

    完成了91题

---
+++ Java/91.java
@@ -0,0 +1,15 @@
+class Problem91{
+    public int numDecodings(String s) {
+        int n = s.length();
+        s = " " + s;
+        char[] arr = s.toCharArray();
+        int[] f = new int[n+1];
+        f[0] = 1;
+        for(int i = 1;i<=n;i++){
+            int a = arr[i] - '0',b = (arr[i-1]-'0')*10 + (arr-'0');
+            if(a>=1&&a<=9)f[i] = f[i-1];
+            if(b>=10&&b<=26)f[i]+=f[i-2];
+        }
+        return f[n];
+    }
+}

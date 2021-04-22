bc53da0c4937723162ec68882c9679e2142c4af7
Yancey <yancey@qq.com>
2021-04-22 21:22:28 +0800

    完成了第91题

--- Java/91.java
+++ Java/91.java
@@ -5,8 +5,8 @@ class Problem91{
         char[] arr = s.toCharArray();
         int[] f = new int[n+1];
         f[0] = 1;
-        for(int i = 1;i<=n;i++){
-            int a = arr[i] - '0',b = (arr[i-1]-'0')*10 + (arr-'0');
+        for(int i=1;i<=n;i++){
+            int a=arr[i]-'0',b = (arr[i-1]-'0')*10 + (arr[i]-'0');
             if(a>=1&&a<=9)f[i] = f[i-1];
             if(b>=10&&b<=26)f[i]+=f[i-2];
         }

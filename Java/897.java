fa7ee64e4bcd24e2f98db6dda0ebe28a3ceab17b
Yancey <yancey@qq.com>
2021-04-27 14:17:36 +0800

    完成了897题

---
+++ Java/897.java
@@ -0,0 +1,47 @@
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+class Problem897{
+    //递归，自己写的渣代码
+    List<Integer> l;
+    public TreeNode increasingBST(TreeNode root) {
+        l = new ArrayList<>();
+        search(root);
+        TreeNode head = new TreeNode();
+        TreeNode node=head;
+        for(int num:l){
+            node.right = new TreeNode();
+            node = node.right;
+            node.val = num;
+        }
+        return head.right;
+    }
+    public void search(TreeNode root){
+        if(root==null)return;
+        search(root.left);
+        l.add(root.val);
+        search(root.right);
+
+    }
+    //第二种
+    List<TreeNode> list = new ArrayList<>();
+    public TreeNode increasingBST0(TreeNode root){
+        dfs(root);
+        TreeNode head = new TreeNode();
+        TreeNode cur = head;
+        for(TreeNode node:list){
+            cur.right = node;
+            node.left = null;
+            cur = node;
+        }
+        return head.right;
+    }
+    private void dfs(TreeNode node){
+        if(node==null)return;
+        dfs(node.left);
+        list.add(node);
+        dfs(node.right);
+    }
+
+}

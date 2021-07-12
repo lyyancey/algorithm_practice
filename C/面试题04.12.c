e24c86609a29b05c9d07689f1e9bc8ba643d43d6
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-07-12 15:13:09 +0800

    Update 面试题04.12.c
    重刷了面试题

--- C/面试题04.12.c
+++ C/面试题04.12.c
@@ -7,25 +7,21 @@
  *     struct TreeNode *right;
  * };
  */
-struct TreeNode {
-      int val;
-      struct TreeNode *left;
-      struct TreeNode *right;
- };
- 
-int count(struct TreeNode* root, int sum){
-    if(root == NULL)return 0;
-    int rootNum = root->val==sum ? 1 : 0;
-    int leftNum = count(root->left,sum-root->val);
-    int rightNum = count(root->right, sum-root->val);
-    return rootNum + leftNum + rightNum;
-    
-};
-
-int pathSum(struct TreeNode* root, int sum){
+//符合当前节点路径的数量
+int count(struct TreeNode *root, int sum){
+    //为空返回0
     if(root==NULL)return 0;
-    int rootNum = count(root, sum);    //以 root 根节点，能凑出几条这样的路径来
-    int leftNum = pathSum(root->left, sum);  //以左子树为根节点可以凑出几条这样的路径来
-    int rightNum = pathSum(root->right, sum);//以右子树为根节点可以凑出几条这样的路径来
-    return rootNum + leftNum + rightNum;
+    //当前节点
+    int curNum = root->val == sum ? 1 : 0;
+    int leftNum = count(root->left, sum - root->val);
+    int rightNum = count(root->right, sum - root->val);
+    return curNum + leftNum + rightNum;
+}
+
+int pathSum(struct TreeNode *root, int sum){
+    if(root==NULL)return 0;
+    int curNum = count(root, sum);
+    int leftNum = pathSum(root->left, sum);
+    int rightNum = pathSum(root->right, sum);
+    return curNum + leftNum + rightNum;
 }

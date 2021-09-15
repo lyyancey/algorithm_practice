d72a082e866cc78a3376092ef12c6453233507ad
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-09-15 10:41:18 +0800

    添加了ListNode公共类

---
+++ Java/ListNode.java
@@ -0,0 +1,7 @@
+public class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(){}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}

8b15be0b09d149177e1e0113a5f18a78b5c8b542
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-09-15 10:22:24 +0800

    重刷了第七题

--- Java/7.java
+++ Java/7.java
@@ -1,17 +1,16 @@
-class Problem7{
+class Solution {
     public int reverse(int x) {
-        int res=0;
-        int tem = 0;
+        int res =0,tem=0;
         while(x!=0){
             tem = x%10;
-            if(res>214748364||(res==2147483647&&tem>7)){
+            if(res>214748364||(res==214748364&&tem>7)){
                 return 0;
             }
-            if(res<-214748364||(res==-2147483647&&tem<-8)){
+            if(res<-214748364||(res==-214748364&&tem<-8)){
                 return 0;
             }
-            res = res*10+tem;
-            x /=0;
+            res=res*10+tem;
+            x/=10;
         }
         return res;
     }

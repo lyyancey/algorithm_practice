193835a4d2ee61ff2d2edbf77d6114f48a9f434f
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-09-15 10:39:29 +0800

    更新了141题

--- Java/141.java
+++ Java/141.java
@@ -1,21 +1,13 @@
-class ListNode {
-     int val;
-     ListNode next;
-     ListNode(int x) {
-         val = x;
-         next = null;
-     }
-}
 class Problem141 {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head,fast = head;
-        while(fast!=null&&fast.next!=null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(fast==slow){
+        ListNode p1 = head, p2 = head;
+        while(p2 != null && p2.next != null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if(p1 == p2){
                 return true;
             }
-        }        
+        }
         return false;
     }
 }

64cb5615b75104ab2aa47b02e969f0333a52d5a8
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-09-15 10:40:56 +0800

    完成了876题

---
+++ Java/876.java
@@ -0,0 +1,10 @@
+class Problem876{
+    public ListNode middleNode(ListNode head){
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}

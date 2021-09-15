118bbda48ebef4b8c47400122a958036c1f893ce
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-09-15 10:21:34 +0800

    更新了第一题

--- Java/1.java
+++ Java/1.java
@@ -1,4 +1,5 @@
-class Solution {
+import java.util.*;
+class Problem1{
     public int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<>();
 		for(int i=0;i<nums.length;i++){

86bd27cd06a202ae7f0a0b53c0e65b24dc224f4e
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-09-23 07:48:12 +0800

    完成了538题

---
+++ Java/538.java
@@ -0,0 +1,16 @@
+class Problem538 {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root){
+        traverse(root);
+        return root;
+    }
+    private void traverse(TreeNode root){
+        if(root == null){
+            return;
+        }
+        traverse(root.right);
+        sum += root.val;
+        root.val = sum;
+        traverse(root.left);
+    }
+}

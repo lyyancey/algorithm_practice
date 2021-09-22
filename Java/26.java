55babd6b12ce7c3c7c9fb9a278ca9271a7e2621b
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-09-23 07:45:46 +0800

    重新刷了26题

--- Java/26.java
+++ Java/26.java
@@ -1,14 +1,11 @@
 class Problem26{
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        int slow = 1, fast = 1;
-        while(fast<n){
-            if(nums[slow-1]!=nums[fast]){
-                nums[slow] = nums[fast];
-                slow++;
+        int slow = 0;
+        for(int fast = 0;fast < nums.length;fast++){
+            if(nums[slow] != nums[fast]){
+                nums[++slow] = nums[fast];
             }
-            fast++;
         }
-        return slow;
+        return slow + 1;
     }
 }

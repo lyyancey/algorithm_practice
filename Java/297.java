7a1695ec6e18ef94fa8fd10f05463c76e8ebe4d2
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-09-29 09:52:02 +0800

    完成了297题

--- Java/297.java
+++ Java/297.java
@@ -79,49 +79,56 @@ class MediaTraverseSolution{
     private String SEP = ",";
     private String NULL = "#";
     public String serialize(TreeNode root){
-        if(root == null)return "";
-        StringBuffer sb = new StringBuffer();
-        //初始化队列，将root加入队列
+        if(root == null) return "";
+        //初始化队列
         Queue<TreeNode> q = new LinkedList<>();
+        //将root节点加入队列
         q.offer(root);
+        //创建StringBuffer保存结果
+        StringBuffer sb = new StringBuffer();
         while(!q.isEmpty()){
-            TreeNode cur = q.poll();
-            if(cur == null){
+            TreeNode node = q.poll();
+            if(node == null){
                 sb.append(NULL).append(SEP);
                 continue;
             }
-            sb.append(cur.val).append(SEP);
-            q.offer(cur.left);
-            q.offer(cur.right);
+            sb.append(node.val).append(SEP);
+            q.offer(node.left);
+            q.offer(node.right);
         }
         return sb.toString();
     }
     public TreeNode deserialize(String data){
         if(data.isEmpty())return null;
+        //所有的节点
         String[] nodes = data.split(SEP);
-        //第一个元素就是root的值
+        //第一个为根节点
         TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
-        //队列q记录父节点，将root加入队列
+        //创建队列，存放所有的上层节点
         Queue<TreeNode> q = new LinkedList<>();
+        //根节点加入队列
         q.offer(root);
-        for(int i=1; i<nodes.length; ){
-            //队列中存在的都是父节点
+        //根据层序遍历顺序，创建二叉树
+        for(int i=1; i<nodes.length;){
+            //从队列中取出父节点
             TreeNode parent = q.poll();
+            //左孩子
             String left = nodes[i++];
-            if(!NULL.equals(left)){
-                parent.left = new TreeNode(Integer.parseInt(left));
-                q.offer(parent.left);
-            }else{
-                parent.left = null;
+            if(!NULL.equals(left)){//如果左孩子不是空节点，就
+                parent.left = new TreeNode(Integer.parseInt(left));//创建新节点，并将父节点的左孩子指向它,并
+                q.offer(parent.left);//讲左孩子加入队列
+            }else{//如果左孩子是空节点, 则
+                parent.left = null;//将父节点的左孩子指向空
             }
-            String right = nodes[i++];
-            if(!NULL.equals(right)){
-                parent.right = new TreeNode(Integer.parseInt(right));
-                q.offer(parent.right);
-            }else{
-                parent.right = null;
+            String right = nodes[i++];//取出右孩子
+            if(!NULL.equals(right)){//如果右孩子不为空
+                TreeNode rightNode = new TreeNode(Integer.parseInt(right));//创建右孩子,并
+                parent.right = rightNode;//将父节点的右孩子指向它，并
+                q.offer(parent.right);//将右孩子加入队列
+            }else{//如果右孩子为空，则
+                parent.right = null;//将父节点的右孩子指向空
             }
         }
-        return root;
+        return root;//返回根节点
     }
 }

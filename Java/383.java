12c6c98e43bd4682ef0645e30b302f3d59c25b20
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-10-13 08:48:45 +0800

    完成了383题

---
+++ Java/383.java
@@ -0,0 +1,15 @@
+class Problem383{
+    public boolean canConstruct(String ransomNote, String magazine){
+        int[] record = new int[26];
+        for(char c : magazine.toCharArray()){
+            record[c - 'a']++;
+        }
+        for(char c : ransomNote.toCharArray()){
+            record[c - 'a']--;
+            if(record[c - 'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}

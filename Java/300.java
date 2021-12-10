31a9aaae833c96491b2b63820f02da0e536de953
yancey <34103258+VictoryNo1@users.noreply.github.com>
2021-12-10 21:40:26 +0800

    添加了第三百题的二分查找的解法

--- Java/300.java
+++ Java/300.java
@@ -16,4 +16,34 @@ class Problem300{
         }
         return res;
     }
+    // 二分查找的方法
+    public int lengthOfLIS(int[] nums){
+        int[] top = new int[nums.length];
+        // 牌堆数初始化为0
+        int piles = 0;
+        for(int i=0; i<nums.length; i++){
+            // 要处理的扑克牌
+            int poker = nums[i];
+            /*********搜索左侧边界的二分查找*********/
+            int left = 0, right = piles;
+            while(left < right){
+                int mid = (left+right) / 2;
+                if(top[mid]>poker){
+                    right = mid;
+                }else if(top[mid]<poker){
+                    left = mid+1;
+                }else{
+                    right = mid;
+                }
+            }
+            /***************************************/
+
+            //没有找到合适的牌堆，新建一堆
+            if(left == piles) piles++;
+            // 把这张牌放到牌堆顶
+            top[left] = poker;
+        }
+        // 牌堆数就是LIS长度
+        return piles;
+    }
 }
